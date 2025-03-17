3e26eaac0ac5750d069cd087b9f383ed4ab2f024
Viral Ganatra <viral@viralganatra.com>
2025-03-17 13:06:33 +0000

    Remove unused env test vars :fire:

--- config/vitest-setup-files.js
+++ config/vitest-setup-files.js
@@ -1,7 +1,5 @@
 import { server } from '../src/mocks/node';
 
-vi.stubEnv('API_KEY', 'API_KEY');
-vi.stubEnv('ACC_NUMBER', 'A-123456');
 vi.stubEnv('POWERTOOLS_LOG_LEVEL', 'silent');
 
 vi.mock('sst', () => {
