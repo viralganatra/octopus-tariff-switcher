785eb23bb01b5c87ddaf5ab64ea9a71e0f0b8490
Viral Ganatra <viral@viralganatra.com>
2025-12-19 11:39:48 +0000

    Upgrade zod to v4 :arrow_up:

--- src/functions/publish-historical-tariff-data/schema.ts
+++ src/functions/publish-historical-tariff-data/schema.ts
@@ -2,48 +2,40 @@ import { z } from 'zod';
 import { snakeToCamelSchema } from '../../utils/schema';
 import { getMsFromApiIsoString } from '../../utils/helpers';
 
+const consumptionItemSchema = z
+  .object({
+    consumption: z.number(),
+    interval_start: z.string(),
+  })
+  .transform(({ consumption, interval_start }) => ({
+    consumptionDelta: consumption * 1000,
+    readAt: interval_start,
+    readAtMs: getMsFromApiIsoString(interval_start),
+  }));
+
 export const schemaConsumption = z.object({
-  results: z
-    .array(
-      z
-        .object({
-          consumption: z.number(),
-          interval_start: z.string(),
-        })
-        .transform(({ consumption, interval_start }) => ({
-          consumptionDelta: consumption * 1000,
-          readAt: interval_start,
-          readAtMs: getMsFromApiIsoString(interval_start),
-        })),
-    )
-    .nonempty(),
+  results: z.tuple([consumptionItemSchema], consumptionItemSchema),
 });
 
 export type ConsumptionIntervals = z.infer<typeof schemaConsumption>['results'];
 
+const agreementSchema = z.object({
+  tariff_code: z.string(),
+  valid_from: z.iso.datetime({ offset: true }),
+  valid_to: z.iso.datetime({ offset: true }),
+});
+
+const meterPointSchema = z.object({
+  agreements: z.tuple([agreementSchema], agreementSchema),
+});
+
+const propertySchema = z.object({
+  electricity_meter_points: z.tuple([meterPointSchema], meterPointSchema),
+});
+
 export const schemaPastTariffs = snakeToCamelSchema(
   z.object({
-    properties: z
-      .array(
-        z.object({
-          electricity_meter_points: z
-            .array(
-              z.object({
-                agreements: z
-                  .array(
-                    z.object({
-                      tariff_code: z.string(),
-                      valid_from: z.string().datetime({ offset: true }),
-                      valid_to: z.string().datetime({ offset: true }),
-                    }),
-                  )
-                  .nonempty(),
-              }),
-            )
-            .nonempty(),
-        }),
-      )
-      .nonempty(),
+    properties: z.tuple([propertySchema], propertySchema),
   }),
 );
 
@@ -51,14 +43,12 @@ export type EletricityAgreements = z.infer<
   typeof schemaPastTariffs
 >['properties'][0]['electricityMeterPoints'][0]['agreements'];
 
+const standingChargeItemSchema = z.object({
+  value_inc_vat: z.number(),
+});
+
 export const schemaStandingCharge = snakeToCamelSchema(
   z.object({
-    results: z
-      .array(
-        z.object({
-          value_inc_vat: z.number(),
-        }),
-      )
-      .nonempty(),
+    results: z.tuple([standingChargeItemSchema], standingChargeItemSchema),
   }),
 );
