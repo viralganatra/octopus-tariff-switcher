7aa099e416583c9b6feeb9c7706f53dbbd3545c6
Viral Ganatra <viral@viralganatra.com>
2026-06-09 20:19:26 +0100

    Upgrade to lambda runtime to node v24 :sparkle:

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -33,7 +33,6 @@ declare module "sst" {
     "octopus-tariff-switcher": {
       "name": string
       "type": "sst.aws.Function"
-      "url": string
     }
     "octopus-tariff-switcher-api": {
       "type": "sst.aws.ApiGatewayV2"
@@ -53,7 +52,6 @@ declare module "sst" {
     }
   }
 }
-/// <reference path="sst-env.d.ts" />
 
 import "sst"
 export {}
