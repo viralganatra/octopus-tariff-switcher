c0b97867b8a9f58b76f0a11fb7c3e884e59f1c9f
Viral Ganatra <viral@viralganatra.com>
2026-06-09 20:19:26 +0100

    Upgrade to lambda runtime to node v24 :sparkle:

--- sst.config.ts
+++ sst.config.ts
@@ -29,7 +29,7 @@ export default $config({
   },
   async run() {
     $transform(sst.aws.Function, (args) => {
-      args.runtime = 'nodejs22.x';
+      args.runtime = 'nodejs24.x';
       args.url ??= $dev ? { authorization: 'iam' } : false;
       args.logging = {
         format: 'json',
